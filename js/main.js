6ef914d086d878d554c2bf1a06fcd3fe97dbd5d3
ldybugs <vitoriaoliveiramedeiros2006@gmail.com>
2023-09-11 14:35:40 -0400

    commit 2

---
+++ js/main.js
@@ -0,0 +1,16 @@
+window.onload = () => {
+    "use strict";
+    if ("serviceWorker" in navigator) {
+      window.addEventListener('load', async () => {
+        try {
+          let reg;
+          reg = await navigator.serviceWorker.register('/sw.js', { type: "module" });
+  
+          console.log('Service worker registrada!', reg);
+        } catch (err) {
+          console.log('Service worker registro falhou: ', err);
+        }
+      })
+    }
+  };
+  
